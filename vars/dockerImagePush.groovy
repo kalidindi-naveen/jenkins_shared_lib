e94991285c28c4fb3f3681a6d0304db6c998ba6f
kalidindi-naveen <151353708+kalidindi-naveen@users.noreply.github.com>
2024-01-11 23:15:51 +0530

    Update dockerImagePush.groovy

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -1,6 +1,6 @@
 def call(String project, String ImageTag, String hubUser){
     withCredentials([usernamePassword(
-            credentialsId: "docker",
+            credentialsId: "docker-tkn",
             usernameVariable: "USER",
             passwordVariable: "PASS"
     )]) {
